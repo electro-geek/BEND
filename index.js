f55f284cabf9ccdc0a416bb57650a613c771c37f
geek-electro <mritunjaypandey0789@gmail.com>
2024-05-14 14:29:34 +0530

    1st command

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('dotenv').config()
+//or
+// import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login at out page</h1>')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('mritunjay.com')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
